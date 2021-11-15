698359e661ae713bc8f0b13935c05853b5a98446
barotiv <88926689+barotiv@users.noreply.github.com>
2021-11-15 16:33:55 +0100

    Fulfilled

---
+++ solutions/ip.js
@@ -0,0 +1,6 @@
+const checkIP = (IP) => {
+    const pattern = /^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
+    return pattern.exec(IP)
+};
+
+export default checkIP;
